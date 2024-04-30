f14997e200140a88799528cd8fd01cefab70df5c
Bedisa Das Bardiya <71769966+bediisad@users.noreply.github.com>
2024-04-30 14:36:12 +0530

    Multiple Kernel

---
+++ 5.cu
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<cuda.h>
+
+__global__ void bdkernel1()
+{
+    printf("Hello World from GPU-1\n");
+}
+
+__global__ void bdkernel2()
+{
+    printf("Hello World from GPU-2\n");
+}
+int main()
+{
+    bdkernel1<<<1,1>>>();
+    bdkernel2<<<1,1>>>();
+
+    printf("CPU\n");
+    cudaDeviceSynchronize();
+
+    return 0;
+}
